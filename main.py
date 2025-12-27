2d1fa059682af5441af59160b930dc476cd579fc
jorawardulai <jorawarsingh.dulai@edu.univ-paris13.fr>
2025-12-27 10:10:05 +0100

    Add numeric label column for ml

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ data = pd.read_csv('data/spam.csv', sep='\t', header=None, names=['label', 'mess
 print(data.head())
 
 print(data.info())
+
+data['label_number'] = data['label'].map({'ham': '0', 'spam': '1'})
+
+print(data[['label', 'label_number']].head())
